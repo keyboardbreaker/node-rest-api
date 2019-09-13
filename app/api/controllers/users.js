3df7a4b3fef2194e5da24a53ff2ae2449d0c4ebf
Yu-Jin Tan <y.tan@trafixiq.com>
2019-09-13 15:47:47 +0100

    creating jwt with jwt-simple

--- app/api/controllers/users.js
+++ app/api/controllers/users.js
@@ -1,10 +1,9 @@
 const userModel = require('../models/users');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 var config = require('../../../config/config');
 var standardResponse = require('../../response/standard-res');
 var errorHandler = require('../../helper/error-handler');
-// var jwt = require('jwt-simple');
+var jwt = require('jwt-simple');
 var moment = require('moment');
 
 module.exports = {
@@ -23,19 +22,32 @@ module.exports = {
         })
     },
     authenticate: function(req, res, next) {
+        var profile = [];
         userModel.findOne({email:req.body.email}, function(err, userInfo){
             if(err || userInfo == null) {
                 res.json({status:"error", message: "Invalid email/password!!!", data:null});
                 next(err);
             } else {
+                profile.push(userInfo['_id']);
                 if (bcrypt.compareSync(req.body.password, userInfo.password)) {
-                    const token = jwt.sign({id: userInfo._id}, req.app.get('secretKey'), { expiresIn: '1h' });
+                    const token = createJWT(userInfo);
                     res.json({ status: "success", message: "user found", data: {user: userInfo, token: token }});
                 } else {
                     res.json({ status: "error", message: "invalid email/password", data: null});
                 }
-
             }
+            
         });
     }
+}
+
+function createJWT(userProfile) {
+    var payload = {
+        sub: userProfile._id,
+        iat: moment().unix(),
+        exp: moment().add(1, 'days').unix(),
+        acc: userProfile.access
+    };
+    console.log(payload);
+    return jwt.encode(payload, config.TOKEN_SECRET);
 }
